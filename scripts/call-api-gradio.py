953668446ba3e66ac333a0240d3e58c41da617b9
alejo-cuello <alejocuello.ib@gmail.com>
2025-12-08 12:33:12 -0300

    [update] readme.md, assets y call-api-gradio.py

--- scripts/call-api-gradio.py
+++ scripts/call-api-gradio.py
@@ -3,12 +3,13 @@ from gradio_client import Client
 client = Client("alejo-cuello/proyecto-final-bootcamp-ds-mlops")
 result = client.predict(
 	param_0=3, # "Cantidad de ambientes"
- 	param_1=2, # "Cantidad de dormitorios"
+ 	param_1=1, # "Cantidad de dormitorios"
 	param_2=1, # "Cantidad de baños"
-	param_3=35, # "Superficie total (m2)"
-	param_4=30, # "Superficie cubierta (m2)"
+	param_3=63, # "Superficie total (m2)"
+	param_4=63, # "Superficie cubierta (m2)"
 	param_5="Capital Federal", # "Zona"
-	param_6="Departamento", # "Tipo de propiedad"
+	param_6="Retiro", # "Barrio"
+	param_7="Departamento", # "Tipo de propiedad"
 	api_name="/predict"
 )
 print(result)
